2ec4b09d71af85e131ebba66c8dc0baf5a255307
Melissa O'Connor <moconnor@factset.com>
2018-07-10 12:59:32 -0400

    feat(fb): add login

---
+++ src/components/Main.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="main">
+    <div class="hello">
+      Hello, {{name}}!
+    </div>
+
+    <div class="submissions">See my submissions</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  data () {
+    return {
+      name: ''
+    }
+  },
+  methods: {
+    init () {
+      // eslint-disable-next-line
+      FB.api('/me', (response) => {
+        console.log(JSON.stringify(response))
+        this.name = response.name
+      })
+    },
+    logout () {
+      // FB.api('/userId/permissions') - need to make delete request to here
+    }
+  },
+  beforeMount () {
+    this.init()
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+.main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.hello {
+  color: rgb(189, 107, 228);
+}
+
+.submissions {
+  height: 250px;
+  width: 250px;
+  background-color: lightblue;
+  text-align: center;
+  vertical-align: middle;
+  line-height: 250px;
+}
+
+.submissions:hover {
+  background-color: rgb(111, 202, 233);
+  color: white;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -11,5 +11,26 @@ new Vue({
   el: '#app',
   router,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  created: () => {
+    /* eslint-disable */
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId            : '452002475265390',
+        autoLogAppEvents : true,
+        xfbml            : true,
+        version          : 'v3.0'
+      });
+
+      FB.AppEvents.logPageView();
+    };
+
+    (function(d, s, id){
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  }
 })
